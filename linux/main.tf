e44ba24ef184f67fc34743d179fa3d5df9e16afe
Greg <gregcourt10@gmail.com>
2025-03-14 09:00:29 +0000

    bugfix

--- linux/main.tf
+++ linux/main.tf
@@ -11,32 +11,35 @@ locals {
     "DeployedBy" = "Cloud Direct"
   }
 
-  # Extract subscription_id from subnet_id using regex
+  # Extract subscription_id and resource group from subnet_id using regex
   subscription_id = element(regex("/subscriptions/([^/]+)/", var.subnet_id), 0)
-  
-  # Get subnet resource to extract location
   subnet_resource_group = element(regex("/resourceGroups/([^/]+)/", var.subnet_id), 0)
   vnet_name = element(regex("/virtualNetworks/([^/]+)/", var.subnet_id), 0)
   subnet_name = element(regex("/subnets/([^/]+)", var.subnet_id), 0)
   
-  # Generate VM name based on location
-  vm_name = "vm-tshoot-${data.azurerm_subnet.this.location}"
+  # Generate VM name based on location from the resource group
+  vm_name = "vm-tshoot-${data.azurerm_resource_group.subnet_rg.location}"
   
   vm_tags_merged = merge(local.common_tags, var.vm_tags)
   rg_tags_merged = merge(local.common_tags, var.rg_tags)
 }
 
-# Get subnet data to extract location
+# Get subnet data
 data "azurerm_subnet" "this" {
   name                 = local.subnet_name
   virtual_network_name = local.vnet_name
   resource_group_name  = local.subnet_resource_group
 }
 
+# Get resource group that contains the subnet to extract location
+data "azurerm_resource_group" "subnet_rg" {
+  name = local.subnet_resource_group
+}
+
 # Create resource group
 resource "azurerm_resource_group" "this" {
   name     = "rg-${local.vm_name}"
-  location = data.azurerm_subnet.this.location
+  location = data.azurerm_resource_group.subnet_rg.location
   tags     = local.rg_tags_merged
 }
 
